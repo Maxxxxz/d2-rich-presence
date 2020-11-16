b4e9da9de65f131a97a48dbc5e95f489713c9a84
Michael Cooper <mc454916@ohio.edu>
2020-11-16 13:00:50 -0500

    grab correct definitions
    add todo notes to remember what I need next

--- Modules/presence.py
+++ Modules/presence.py
@@ -24,14 +24,20 @@ class D2Presence:
         man = requests.get(BASE_URL + "Manifest/")
         self.manifest = man.json()
         # print(j1["Response"]["jsonWorldComponentContentPaths"]['en']['DestinyActivityDefinition'])
-        hashes = requests.get("http://bungie.net" + self.manifest["Response"]["jsonWorldComponentContentPaths"]['en']['DestinyActivityDefinition'])
+        # hashes = requests.get("http://bungie.net" + self.manifest["Response"]["jsonWorldComponentContentPaths"]['en'])
         # for x in j1["Response"]:
         #     print(x)
         # print(j1["Response"]["jsonWorldComponentContentPaths"]["en"]['DestinyDestinationDefinition'])
-        self.activity_hashes = hashes.json()
+        # self.hashes = hashes.json()
+        self.activity_hashes = requests.get("http://bungie.net" + self.manifest["Response"]["jsonWorldComponentContentPaths"]["en"]["DestinyActivityDefinition"]).json()
+        self.destin_hashes = requests.get("http://bungie.net" + self.manifest["Response"]["jsonWorldComponentContentPaths"]["en"]["DestinyPlaceDefinition"]).json()
+        self.activity_type_hashes = requests.get("http://bungie.net" + self.manifest["Response"]["jsonWorldComponentContentPaths"]["en"]["DestinyActivityTypeDefinition"]).json()
 
-        destin_hashes = requests.get("http://bungie.net" + self.manifest["Response"]["jsonWorldComponentContentPaths"]["en"]['DestinyDestinationDefinition'])
-        self.dest_hashes = destin_hashes.json()
+        # for x in self.activity_hashes:
+        #     print(x)
+
+        # destin_hashes = requests.get("http://bungie.net" + self.manifest["Response"]["jsonWorldComponentContentPaths"]["en"]['DestinyDestinationDefinition'])
+        # self.dest_hashes = destin_hashes.json()
         # This gets the name for the activity hash, will also contain the location hash
         # for x in j2:
             # print(j2[x]["destinationHash"])
@@ -73,10 +79,13 @@ class D2Presence:
 
     def getLatestActivity(self, injson):
 
-        # logic to find most recent activity from activities
-        # stub to use first result
+        act = "null"
+        act_type = "null"
 
-        # print(injson['Response'][])
+        # todo
+        # get place hash
+        # get destination hash
+        # change how rich presence is done with playlist activities
 
         timestamps = {}
         tempjson = injson['Response']["characterActivities"]["data"]
@@ -91,17 +100,48 @@ class D2Presence:
 
         # ENDED HERE: trying to find the current activity has to then translate into the name of activity
 
-        activity_hash = tempjson[max(timestamps, key=lambda key: timestamps[key])]
-        # current_mode_type = activity_hash["currentActivityModeType"]
+        # for x in tempjson:
+        #     print(x)
+
+        currentjson = tempjson[max(timestamps, key=lambda key: timestamps[key])]
+
+        activity_hash = currentjson["currentActivityHash"]
         
+        # activity_hash = tempjson["currentActivityModeHash"]
+        current_mode_hash = currentjson["currentActivityModeHash"]
+        current_mode_type = currentjson["currentActivityModeType"]
+
         print(activity_hash)
+        print(current_mode_hash)
+        print(current_mode_type)
+
+        for x in self.activity_type_hashes:
+            if str(x) == str(current_mode_hash):
+                print("match on type!")
+                # print(self.activity_type_hashes[x]['displayProperties']['name'])
+                act_type = self.activity_type_hashes[x]['displayProperties']['name']
+
+
+        for x in self.activity_hashes:
+            if str(x) == str(activity_hash):
+                print("match on activity!")
+                # print(self.activity_hashes[x]['displayProperties']['name'])
+                act = self.activity_hashes[x]['displayProperties']['name']
+
+        # for x in self.:
+        #     if str(x) == str(activity_hash):
+        #         print("match!")
+        #         print(self.activity_hashes[x]['displayProperties']['name'])
+
+        # print(self.activity_type_hashes[current_mode_hash]['name'])
         # print(self.activity_hashes[current_mode_type])
 
+        # print(activity_hash)
+        # print(self.activity_hashes[activity_hash])
+
         dataToReturn = [
-            # self.activity_hashes[activity_hash]['name'],
-            "name",
-            # self.activity_hashes[current_mode_type],
-            "description",
+            act,  # State
+            act_type,      # Details
             "ta",
             "ab",
             "ba",
@@ -119,7 +159,8 @@ class D2Presence:
         return dataToReturn
 
     def getActivitiesJSON(self):
-        t = requests.get(BASE_URL + "{0}/Profile/{1}/?components=CharacterActivities".format(platform, memID, charID), headers=my_header)
+        # t = requests.get(BASE_URL + "{0}/Profile/{1}/?components=CharacterActivities".format(platform, memID, charID), headers=my_header)
+        t = requests.get(BASE_URL + "{0}/Profile/{1}/?components=204".format(platform, memID, charID), headers=my_header)
         j = t.json()
         return j
 
