9fea0dfde4b1a52f5859fc00713141b5c7f5b753
Michael Cooper <mc454916@ohio.edu>
2021-01-07 23:12:04 -0500

    Create content for GetInfo page

--- Modules/Pages.py
+++ Modules/Pages.py
@@ -26,9 +26,38 @@ class Menu(Page):
 
 #   First Load/Reset Page
 class GetInfo(Page):
+
     def __init__(self, w, RPC):
         Page.__init__(self)
-        label = tk.Label(self, text="What platform are you using?")
-        label.place(x=(w/2), y=25, anchor="center")  # Place label at top of screen
+
+        self.PLATFORMS = ["PC", "Playstation", "XBox", "Stadia"]
+
+        self.topLabel = tk.Label(self, text="Enter your information here.")
+        self.topLabel.place(x=(w/2), y=25, anchor="center")  # Place label at top of screen
+
+        self.memTypeLabel = tk.Label(self, text="What platform do you use?")
+        self.memTypeLabel.place(x=(w/2), y=55, anchor="center")
+
+        self.curType = tk.StringVar(self)
+        self.curType.set(self.PLATFORMS[0])
+
+        self.typeBox = tk.OptionMenu(self, self.curType, *self.PLATFORMS)
+        self.typeBox.place(x=(w/2), y=85, anchor="center")
+
+
+        self.memLabel = tk.Label(self, text="What is your ID?")
+        self.memLabel.place(x=(w/2), y=115, anchor="center")
+
+        self.idField = tk.Entry(self)
+        self.idField.place(x=(w/2), y=145, anchor="center")
+
+
+
+        self.submit = tk.Button(self, text="Submit", command= self.onSubmit )
+        self.submit.place(x=(w/2), y=200, anchor="center")
+
+    # validate info (check ID and memtype return valid player)
+    def onSubmit(self):
+        print("platform is {}".format(self.curType.get()))
 
 #   Make Pages for each system login (ie, pc page, ps page, xbox page, etc)
